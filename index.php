ca5676791332baa5793ace38dd0ee2f5e7d0ac70
erwingituab <erwin.apace@uab.edu.bo>
2025-03-14 18:08:12 -0400

    C.N. Libreria -> Framework

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Vista/Libreria/Bootstrap4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Vista/Framework/Bootstrap4/css/bootstrap.min.css">
     <link rel="stylesheet" href="Vista/Pagina/css/style.css">
     <title>Login</title>
 </head>
@@ -12,9 +12,7 @@
     require "Controlador/CtrlLogin.php";
     $mostrar = CtrlLogin::MotrarLogin();
     echo $mostrar;?>
-    <script src="Vista/Libreria/jQuery/jquery-3.7.1.min.js"></script>
-    <script src="Vista/Libreria/Bootstrap4/js/bootstrap.min.js"></script>
-    <!--<script src="Vista/jQuery/jquery-3.7.1.min.js"></script>
-    <script src="Vista/pagina/js/Login.js"></script>-->
+    <script src="Vista/Framework/jQuery/jquery-3.7.1.min.js"></script>
+    <script src="Vista/Framework/Bootstrap4/js/bootstrap.min.js"></script>
 </body>
 </html>
